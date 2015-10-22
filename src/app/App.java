d60bf3d3ecaaedecc0a57e3b3246b72d4adf1b1b
Daniel Roach <dannycr09@hotmail.com>
2015-10-22 17:07:42 -0400

    Edited App.java so that it extends JavaFX Application

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,14 @@
 package app;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+   @Override
+   public void start(Stage primaryStage) throws Exception {
+      // TODO Auto-generated method stub
+      
+   }
 
 }
