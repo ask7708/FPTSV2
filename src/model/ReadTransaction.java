ed92b3bbc84ce992bdbbf83ce007e8c6be6d51b5
Daniel Cypher <dcc7331@g.rit.edu>
2015-10-26 10:34:59 -0400

    initial commit of Readtransaction

---
+++ src/model/ReadTransaction.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ReadTransaction {
+	
+	//Transaction Format
+	//Transaction id, Ticker (Equity or Account), Name of Equity/Account
+	//CurrentValue, #Shares (amount for account), Routing num, bankName
+	
+	public Transaction readTransaction(String[] temp){
+		if(temp[1] == "!bank"){
+			
+		}
+		else{
+			
+		}
+		return null;
+	}
+}
