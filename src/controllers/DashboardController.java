c5d8f50ec35770cc5a50b10ae2e1f9e457963d31
itnks <tna3531@rit.edu>
2015-10-26 21:27:05 -0400

    commting dashboard controller

---
+++ src/controllers/DashboardController.java
@@ -0,0 +1,56 @@
+package controllers;
+import java.io.BufferedWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import app.LoginMainApp;
+import model.User;
+//import ch.makery.login.model.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class DashboardController {
+	
+	public DashboardController() {
+		// TODO Auto-generated constructor stub
+
+	}
+	
+	@FXML private Button logoutB;
+	
+	
+	
+	
+	public void logoutHandler(ActionEvent even) throws IOException{
+	    Stage stage = null; 
+	    Parent root = null;
+	    // if(even.getSource()==logoutButton){        
+			  stage=(Stage) logoutB.getScene().getWindow();
+			  root = FXMLLoader.load(getClass().getResource("../views/LoginView.fxml"));
+	      //}
+	      Scene scene = new Scene(root);
+	      stage.setScene(scene);
+	      stage.show();
+	    }
+	
+}
