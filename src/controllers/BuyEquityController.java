ea8e618df5226f59adf16729c5ca214e7527ec6b
ask7708 <ask7708@rit.edu>
2015-11-03 04:49:14 -0500

    Added controller to handle buying equities -*not yet full implemented*

---
+++ src/controllers/BuyEquityController.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import model.Equity;
+
+/**
+ * @author Arsh
+ *
+ */
+public class BuyEquityController {
+
+	@FXML
+	private Button buyButton;
+	
+	@FXML
+	private Button cancelButton;
+	
+	@FXML
+	private Label tickSymbolToBeSetLabel;
+	
+	@FXML
+	private Label equityNameToSetLabel;
+	
+	@FXML
+	private Label priceToSetLabel;
+	
+	private Stage dialogStage;
+	
+	private MarketController markControl;
+	
+	private Equity selectedEq;
+	
+	public void initialize(){
+		
+		
+		
+		
+	}
+	
+	@FXML
+	public void cancelPressed(){
+		
+		dialogStage.close();
+	}
+	
+	public void setDialogStage(Stage dialogStage){
+		
+		this.dialogStage = dialogStage;
+	}
+
+	public void setMarketController(MarketController marketController) {
+		
+		this.markControl = marketController;
+		
+	}
+	
+	public void setTickSymbolLabel(String ticker){
+		
+		tickSymbolToBeSetLabel.setText(ticker);
+	}
+	
+	public void setEquityNameLabel(String name){
+		
+		equityNameToSetLabel.setText(name);
+	}
+
+	public void setPriceLabel(String price){
+	
+	priceToSetLabel.setText(price);
+	}
+	
+	
+}
